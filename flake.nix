948866018fd34bed0607922f8304b461276633bf
Eddie Nguyen <edward_nguyen@brown.edu>
2024-09-07 13:02:08 -0400

    working template?

--- flake.nix
+++ flake.nix
@@ -1,35 +1,6 @@
 {
-  description = "A very basic flake";
-
-  inputs.utils.url = "github:numtide/flake-utils";
-
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
-        templates.tex.path = ./.;
-
-        devShells.default = pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            just
-            tectonic
-            zathura
-            perl538Packages.LatexIndent
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-
-          shellHook = ''
-            just watch 2>&1 > /dev/null &
-            zathura build/default/default.pdf 2>&1 > /dev/null &
-            trap "./exit.sh" EXIT
-          '';
-        };
-      }
-    );
+  outputs = {self}: {
+    templates.tex.path = ./tex;
+    defaultTemplate = self.templates.tex;
+  };
 }
