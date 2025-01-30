7ce9120bc47e225cd12ee4f8153cc85d9d4edac7
Eddie Nguyen <edward_nguyen@brown.edu>
2025-01-30 15:15:19 -0500

    make template usable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,17 @@
 {
+  description = "Some of my flake templates";
+
   outputs = {self}: {
-      templates.default.path = ./tex;
+      templates = {
+          tex = {
+              path = ./tex;
+              description = "A template with a shell hook for LaTeX work";
+          };
+
+          typst = {
+              path = ./typst;
+              description = "A template with a shell hook for Typst work";
+          };
+      };
   };
 }
